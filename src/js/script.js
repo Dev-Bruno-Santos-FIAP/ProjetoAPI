a0d9da7b999a84d1b9d959d037a5ab8b04f95b00
Bruno Carreiro <brunocarreirodossantos12@gmail.com>
2026-05-14 09:45:45 -0300

    fix: arrumando estrutura js

--- src/js/script.js
+++ src/js/script.js
@@ -1,71 +1,67 @@
 //Declarações dos Elementos usando DOM(Document Object Model)
-const videoElemento = document.getElementById('video');
-const botaoScanear = document.getElementById('btn-texto');
-const resultado = document.getElementById('saida');
-const canvas = document.getElementById('canvas');
-
+const videoElemento = document.getElementById("video");
+const botaoScanear = document.getElementById("btn-texto");
+const resultado = document.getElementById("saida");
+const canvas = document.getElementById("canvas");
 
 //Função assincrona para habilitar a câmera
 async function configurarCamera(){
-    //tratamento de erros usando try catch
+    //tratamento de erros
     try{
-    //chama a api do navegador para solicitar acesso 
+        //chama a api do navegador para solicitar acesso
         const midia= await navigator.mediaDevices.getUserMedia({
-            //habilit a camera traseira
-            video: { facingMode: 'environment' }
-            //o audio nao sera capturado
+            //habilit a câmera traseira
+            video:{ facingMode: "environment"},
+            //o audio não será capturado
             audio:false
         });
-        //recebe a função midia para ser executada 
-        videoElemento.srcObject = midia;
-        //fprça a reprodução do video
+        //recebe a função midia para ser executada
+        videoElemento.srcObject=midia;
+        //força a reprodução do video
         videoElemento.play();
 
-    } catch(error){
-        resultado.innerText = 'Erro ao acessar a câmera: ',erro;
+    }catch(erro){
+        resultado.innerText="Erro ao acessar a Câmera",erro;
     }
 }
 //executando a função
 configurarCamera();
 
-//função para capturar o texto da camera
-botaoScanear.onclick =async await () =>{
-    botaoScanear.disabled = true; //habilitando a camera
-    resultado.innerText = 'Fazendo a Leitura do texto, aguarde...';
+//função para capturar o texto da câmera
+botaoScanear.onclick =async ()=>{
+     botaoScanear.disabled=true; // habilitando a camera
+     resultado.innerText="Fazendo a leitura do texto...aguarde";
 
-    //Define o canvas para iniciar a leitura
-    const contexto = canvas.getContext('2d');
+     //Define o canvas para iniciar a leitura
+     const contexto = canvas.getContext("2d");
 
-    //Ajusta o tamanho do canvas para o mesmo do video
-    canvas.width = videoElemento.videoWidth;
-    canvas.height = videoElemento.videoHeight;
+     //ajusta o tamanho do canvas para o tamanho real do video
+     canvas.width =videoElemento.videoWidth;
+     canvas.height = videoElemento.videoHeight;
 
-    //aplica o filtro para melhorar o OCR
-    contexto.filter='contrast(1.2) grayScale(1)';
+     //aplica o filtro para melhorar o OCR
+     contexto.filter='contrast(1.2) grayscale(1)';
 
      //desenha o video no canvas
 
      contexto.drawImage(videoElemento,0,0,canvas.width,canvas.height);
 
      try{
-        const {data:{text}}= await Tesseract.recognize(
+        const {data:{ text }}=await Tesseract.recognize(
             canvas,
-            'por'//define o idioma para português
+            'por' //define o idioma
         );
-        const textoFinal = text.trim(); //remove os espaços em branco
-        //estrutura conficional ternaria ? =if : =else
-        resultado.innerTex =textoFinal.length > 0? textoFinal : 'Não foi possivel indentificar o texto.';
-
+        //remove os espaços em branco
+        const textoFinal = text.trim();
+        //estrutura condicional ternaria ? = if : =else
+        resultado.innerText=textoFinal.length > 0 ?textoFinal: "Não foi possivel identificar o texto";
+        
      }catch(erro){
-        resultado.innerText = 'Erro ao processar a imagem: ',erro;
-
+        resultado.innerText="Erro no processamento",erro
      }
-     finally{
-        botaoScanear.disabled = false; //desabilitando o botão para nova captura
-
+     finally{ 
+        //desabilita o botão para fazer nova captura
+            botaoScanear.disabled=false; 
      }
-    
-
-
 
 }
